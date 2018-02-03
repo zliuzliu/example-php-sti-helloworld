d0d125d31a0d5d490e693247706b1edb0d3a0444
zliuzliu <zliu@msn.cn>
2018-02-03 15:59:39 +0800

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,18 +1,22 @@
 <?php
-class getIp(){
-     function ClientIp(){
-    $cIP= getenv($_SERVER['REMOTE_ADDR']);
-    $cIP1 = getenv($_SERVER['HTTP_X_FORWORD_FOR']);
-    $cIP2 = getenv($_SERVER['HTTP_CLIENT_IP']);
-   $cIP1?$cIP= $cIP1?null;
-    $cIP2?$cIP = $cIP2?null;
-   return $cIP;
-}
-     function serverIP(){
-       return gethostbyname($_SERVER['SERVER_NAME']);
-}
-}
-$getIP  = new getIP();  
-$clientIp = getIP::clientIP();  
-$serverIp = getIP::serverIP();  
+class getIP{     
+ function clientIP(){     
+  $cIP = getenv('REMOTE_ADDR');     
+  $cIP1 = getenv('HTTP_X_FORWARDED_FOR');     
+  $cIP2 = getenv('HTTP_CLIENT_IP');     
+  $cIP1 ? $cIP = $cIP1 : null;     
+  $cIP2 ? $cIP = $cIP2 : null;     
+  return $cIP;     
+ }     
+ function serverIP(){     
+  return gethostbyname($_SERVER["SERVER_NAME"]);     
+ }     
+}     
+      
+$getIP  = new getIP();     
+$clientIp = getIP::clientIP();     
+$serverIp = getIP::serverIP();     
+      
+echo 'Client IP is ',$clientIp,'<br />';     
+echo 'Server IP is ',$serverIp,'<br />';  
 ?>
